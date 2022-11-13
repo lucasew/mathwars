ee207d109d60e1d505f7c84ef1f127ac99212864
lucasew <lucas59356@gmail.com>
2022-11-13 09:08:51 -0300

    rascunho da página inicial
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -16,19 +16,40 @@
         font: Cursive;
     }
     .progress-bar-container {
-        height: 10px;
+        height: 1rem;
         width: 100%;
         border: 1px solid white;
+        box-sizing: border-box;
+    }
+    .mathwars-logo {
+        max-width: 80vw;
+    }
+    .mathwars-button {
+        background-color: white;
+        width: 100%;
+        border: 0;
+        padding: 0.5rem 0;
+        margin: 0.2rem 0;
+        text-align: center;
+        color: inherit;
+        text-decoration: none;
+        font-size: 1rem;
+        vertical-align: middle;
+        line-height: normal;
+    }
+    .mathwars-page-section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 </style>
 
 <main>
-    <section>
-        <img src="/mathwars.png">
+    <section class="mathwars-page-section">
+        <img class="mathwars-logo" src="/mathwars.png">
         <p>master of numbers</p>
-        <center>
-            <Spinner />
-        </center>
+        <button class="mathwars-button">Iniciar</button>
+        <a href="/eoq" class="mathwars-button">Teste</a>
         <div class="progress-bar-container">
             <ProgressBar progress="10" />
         </div>
