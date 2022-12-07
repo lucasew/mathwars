a81f65fe2ef742e6155dbc1cd201ceeecdaea0e8
lucasew <lucas59356@gmail.com>
2022-12-07 15:13:40 -0300

    acabamento com opacidade
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/lib/MathwarsLogo.svelte
+++ src/lib/MathwarsLogo.svelte
@@ -1,5 +1,8 @@
 <img class="mathwars-logo" src="/mathwars.png" on:click>
 
+<svelte:head>
+    <link rel="prefetch" href="/mathwars.png" />
+</svelte:head>
 <style>
 .mathwars-logo {
     max-width: 80vw;
